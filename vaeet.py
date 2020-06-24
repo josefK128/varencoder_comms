cb25e4c5d271a470de8d30124b436eab05235741
Mark Rudolph <mark_rudolph@yahoo.com>
2020-06-24 08:41:34 -0700

    makes mod to vaeet.py to allow mixed file-types in directories of email json-files. Also creates two additional phishing emails in original db/phishing2 and synthesized db_/phishing2 - phishing2 is the full collection of phishing examples.

--- vaeet.py
+++ vaeet.py
@@ -55,55 +55,58 @@ directory_ = './db_/' + thread
 
 def action(diagnostics:bool=False) -> None:
     for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
-        filepath_ = os.path.join(directory_, filename)
-        print('---------------------------------------------------')
-        print('filepath = ' + filepath)
-        print('filepath_ = ' + filepath_)
+        if filename.endswith('.json'):
+            filepath = os.path.join(directory, filename)
+            filepath_ = os.path.join(directory_, filename)
+            print('---------------------------------------------------')
+            print('filepath = ' + filepath)
+            print('filepath_ = ' + filepath_)
     
-        # parse email file
-        with open(filepath, 'r') as f:
-            data = json.load(f)
-            msg = data['msg']
-            msg_ = ''
-            data_ = {}
-
-
-            # separate sentences from msg of data object
-            sentences = msg2s.split_into_sentences(msg)
-            if diagnostics:
-                print('\nvaee: sentences in msg = ' + str(sentences))
-
-
-            # substitute synonyms for word in each sentence and return both
-            sentences_, words_ = synonyms.replace(sentences, maxsubs)
-            if diagnostics:
-                print('\n\nvaee: *** after synonym substitution sentences are:')
-                for index in range(len(sentences)):
-                    print('\nwords_['+str(index)+'] = ' + str(words_[index]))
-                    print('sentence['+str(index)+'] = ' + sentences[index])
-                    print('sentence_['+str(index)+'] = ' + sentences_[index])
-
-
-            # prepare json-container data_ for new email with the same content 
-            # as the ingested json-container data, but altered msg
-            data_['sender'] = data['sender'] or ''
-            data_['receivers'] = data['receivers'] or [] 
-            data_['sent'] = data['sent'] or ''
-            data_['subject'] = data['subject'] or '' 
-            msg_ = msg_.join(sentences_)
-            data_['msg'] = msg_
-            if diagnostics:
-                print("\nvaee: data_['msg'] = " + data_['msg'])
-
-
-            # write data_ as json to db_ at filepath_ 
-            data_json = json.dumps(data_, indent = 4, sort_keys=True)
-            with open(filepath_, 'w+') as f:
-                f.write(data_json)
-                f.close()
+            # parse email file
+            with open(filepath, 'r') as f:
+                data = json.load(f)
+                msg = data['msg']
+                msg_ = ''
+                data_ = {}
+    
+    
+                # separate sentences from msg of data object
+                sentences = msg2s.split_into_sentences(msg)
                 if diagnostics:
-                    print("\nvaee: wrote data_json to " + filepath_)
+                    print('\nvaee: no. of sentences in msg = ' + str(len(sentences)))
+                    print('\nvaee: sentences in msg = ' + str(sentences))
+    
+    
+                # substitute synonyms for word in each sentence and return both
+                sentences_, words_ = synonyms.replace(sentences, maxsubs)
+                if diagnostics:
+                    print('\n\nvaee: *** after synonym substitution sentences are:')
+                    for index in range(len(sentences)):
+                        print('\nwords_['+str(index)+'] = ' + str(words_[index]))
+                        print('sentence['+str(index)+'] = ' + sentences[index])
+                        print('sentence_['+str(index)+'] = ' + sentences_[index])
+    
+    
+                # prepare json-container data_ for new email with the same content 
+                # as the ingested json-container data, but altered msg
+                data_['sender'] = data['sender'] or ''
+                data_['receivers'] = data['receivers'] or [] 
+                data_['sent'] = data['sent'] or ''
+                data_['subject'] = data['subject'] or '' 
+                msg_ = msg_.join(sentences_)
+                data_['msg'] = msg_ 
+                if diagnostics:
+                    print("\nvaee: data_['msg'] = " + data_['msg'])
+    
+    
+                # write data_ as json to db_ at filepath_ 
+                data_json = json.dumps(data_, indent = 4, sort_keys=True)
+                with open(filepath_, 'w+') as f:
+                    f.write(data_json)
+                    f.write('\n')
+                    f.close()
+                    if diagnostics:
+                        print("\nvaee: wrote data_json to " + filepath_)
 
  
 
